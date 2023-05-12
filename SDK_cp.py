c79c387f8b6b7a20fd0aaca2fc91cb7e392a5f9b
shaojiajun314 <63770117@qq.com>
2023-05-12 14:13:54 +0800

    update

--- SDK_cp.py
+++ SDK_cp.py
@@ -1,11 +1,14 @@
 import sys
+import asyncio
 import embit
 import bitcoin
 import requests
-from json import loads
 from requests.cookies import cookiejar_from_dict
 from time import sleep
 from embit.psbt import PSBT
+
+from playwright.async_api import async_playwright
+from cf_clearance import async_cf_retry, async_stealth
 
 
 # from binascii import unhexlify
@@ -177,6 +180,10 @@ def retry(func):
 					print('http 429 retry')
 					sleep(SLEEP_SECONDS)
 					continue
+				if e.status_code == 403:
+					print('challenge')
+					arg[0].challenge_cloudflare()
+					continue
 				raise
 
 		print('429 retry too many')
@@ -222,16 +229,15 @@ class SDK():
 		private_key,
 		address=None,
 		cookies=None,
-		tick_path='ticks.json'
+		chrome_path=None
 		# address_type=ADDRESS_TYPES.P2WPKH,
 	):
 		self.private_key = private_key
 		self.private_key_for_sign = embit.ec.PrivateKey.from_wif(private_key)
 		self.public_key = bitcoin.privkey_to_pubkey(private_key)
+		self.chrome_path = chrome_path
 		self.session = requests.Session()
 		self.session.cookies = cookiejar_from_dict(cookies or {})
-		with open(tick_path) as f:
-			self.ticks = loads(f.read())
 		if address:
 			self.address = address
 		else:
@@ -241,16 +247,91 @@ class SDK():
 			raise
 			# if address_type == ADDRESS_TYPES.P2WPKH:
 			# 	self.address = key_to_p2wpkh(self.public_key, main=True)
+
+	async def __challenge_cloudflare(self):
+		async with async_playwright() as p:
+			browser = await p.chromium.launch(
+				executable_path=self.chrome_path,
+				headless=False,
+				chromium_sandbox=False
+			)
+			page = await browser.new_page()
+			await async_stealth(page, pure=False)
+			await page.goto('https://unisat.io')
+			success = await async_cf_retry(page)
+			assert success, 'todo error'
+			# user_agent = await page.evaluate("() => navigator.userAgent")
+			cookies = {
+				cookie["name"]: cookie["value"]
+				for cookie in await page.context.cookies()
+			}
+			print(cookies)
+			self.session.cookies = cookiejar_from_dict(cookies)
+
+	def challenge_cloudflare(self):
+		asyncio.run(self.__challenge_cloudflare())
+	@retry
+	def a(self, tick, start, limit):
+		r = r = self.session.get(
+			f'https://unisat.io/brc20-api-v2/brc20/status?ticker={tick}&start={start}&limit={limit}&complete=&sort=transactions',
+			headers={
+				'User-Agent':
+				'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+			}
+		)
+		if r.status_code != 200:
+			raise HTTP_ERROR(msg='brc20_types error', status_code=r.status_code)
+		ret = r.json()
+		if ret['code'] != 0:
+			raise HTTP_ERROR(msg='brc20_types error', status_code=r.status_code)
+		return ret
+
+	def test(self, tick):
+		start = 0
+		limit = 500
+		ticks = set()
+		while 1:
+			ret = self.a(tick, start, limit)
+			ticks = ticks.union({i['ticker']
+				for i in ret['data']['detail']})
+			start += limit
+			if ret['data']['total'] <= start:
+				break
+			
+		return ticks
 	
 	@retry
 	def brc20_types(self, tick=None):
 		if tick:
+			r = self.session.get(
+				f'https://unisat.io/brc20-api-v2/brc20/status?ticker={tick}&start=0&limit=10&complete=&sort=transactions',
+				headers={
+					'User-Agent':
+					'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'
+				}
+			)
+			if r.status_code != 200:
+				raise HTTP_ERROR(msg='brc20_types error', status_code=r.status_code)
+			ret = r.json()
+			if ret['code'] != 0:
+				raise HTTP_ERROR(msg='brc20_types error', status_code=r.status_code)
 			return [
-				i
-				for i in self.ticks
-				if i.find(tick) >= 0
+				i['ticker']
+				for i in ret['data']['detail']
 			]
-		return self.ticks
+
+		r = self.session.post(
+			f'{self.base_url}/auction/brc20_types',
+		)
+		if r.status_code != 200:
+			raise HTTP_ERROR(msg='brc20_types error', status_code=r.status_code)
+		ret = r.json()
+		if ret['code'] != 0:
+			raise HTTP_ERROR(msg='brc20_types error', status_code=r.status_code)
+		return [
+			i['tick']
+			for i in ret['data']['list']
+		]
 
 	@retry
 	def __auction_list(self, tick, s, l):
@@ -348,9 +429,11 @@ if __name__ == '__main__':
 	address =os.getenv('address') or None
 	print(address)
 
+
 	sdk = SDK(
 		private_key,
 		address=address,
+		chrome_path="C:\Program Files\Google\Chrome\Application\chrome.exe"
 	)
 	print(sdk.brc20_types())
 	print(sdk.brc20_types('shi'))
@@ -360,6 +443,52 @@ if __name__ == '__main__':
 	# 		print(i)
 	# 		break
 	# 	print('not')
+
+	ticks = set()
+	for i in [
+		'a',
+		'b',
+		'c',
+		'd',
+		'e',
+		'f',
+		'g',
+		'h',
+		'i',
+		'j',
+		'k',
+		'l',
+		'm',
+		'n',
+		'o',
+		'p',
+		'q',
+		'r',
+		's',
+		't',
+		'u',
+		'v',
+		'w',
+		'x',
+		'y',
+		'z',
+		'1',
+		'2',
+		'3',
+		'4',
+		'5',
+		'6',
+		'7',
+		'8',
+		'9'
+	]:
+		ticks = ticks.union(sdk.test(i))
+		print(len(ticks))
+	print(ticks)
+	from json import dumps
+	f = open('ticks.json', 'w')
+	f.write(dumps(tuple(ticks)))
+	f.close()
 	# i = next(sdk.auction_list('0shi'))
 	# print('*' * 20)
 	# sdk.bid(i)
